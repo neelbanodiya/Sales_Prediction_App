fe08ef6b79fa0bf4c067b339d7b5fa2cbd4cbe20
neelbanodiya <77720586+neelbanodiya@users.noreply.github.com>
2024-01-03 11:01:25 +0530

    Add files via upload

---
+++ 1_🏡_Homepage.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+
+st.title('Welcome to the Streamlit App')
+
+st.subheader("This application predict the sales based on the machine learning model trained on Training Data")
+
+
+
+st.subheader("Click on the sidbar to check out other pages")
+
+with st.sidebar:
+    st.success("The app is successfully loaded")
+    st.info("Made by: Neel and Pariket 😊")
